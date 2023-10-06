be967edee44ec421b9436e1bf2039edeea25567d
Caleb Solomon <cj5bossprofessional@gmail.com>
2023-10-06 15:12:13 -0400

    reformatting

--- ntr/AgeMeasureAnalysis.R
+++ ntr/AgeMeasureAnalysis.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(dplyr)
 
+# Set directory to the root directory of the project folder
 # Desktop
 setwd("C:/Users/Caleb Solomon/Documents/GitHub/ROAR-LDT-Public")
 
@@ -18,10 +19,10 @@ wide_newcodes_data = read.csv("data_allsubs/LDT_alldata_wide_newcodes.csv")
 # specified. Rounds all values in the list accordingly, and removes extra
 # whitespace in the elements of the list.
 round_list <- function(list, digits) {
-  rounded_list <- lapply(list, function(elt) {
-    trimws(format(round(elt, digits), nsmall = digits))
-  })
-  return(rounded_list)
+    rounded_list <- lapply(list, function(elt) {
+        trimws(format(round(elt, digits), nsmall = digits))
+    })
+    return(rounded_list)
 }
 
 # Changing metadata to reflect age in years
@@ -44,10 +45,11 @@ updated_long_newcodes <- long_newcodes_data %>%
 # data is already sorted in ascending subject ID order). Then filter.
 subj <- 1:120
 updated_wide_newcodes <- data.frame(subj, wide_newcodes_data) %>%
-  filter(subj %in% updated_metadata$subj)
+    filter(subj %in% updated_metadata$subj)
 
 # Add age data to the updated_long_newcodes next to each subject
-updated_long_newcodes <- merge(updated_long_newcodes, updated_metadata[, c("subj", "visit_age")], by = "subj", all.x = TRUE)
+updated_long_newcodes <- merge(updated_long_newcodes, 
+    updated_metadata[, c("subj", "visit_age")], by = "subj", all.x = TRUE)
 
 # Saves this data to an output file
 # write.csv(updated_long_newcodes, "updated_long_newcodes.csv")
@@ -66,54 +68,59 @@ dir.create("age_data")
 # each age bin. The resultant list should then be passed into get_word_age_accuracies() below
 # whenever it is called. 
 for (age in seq(from = ceiling(min_age), to = ceiling(max_age), by = 1)) {
-  print("age")
-  print(age)
-  temp_df <- data.frame()
-  out_path <- paste0("age_data/bin_", age - 1, ".csv")
-  # Iterate through all subjects
-  for (subject in updated_metadata$subj) {
-    # If that subject is within the desired range
-    if (!is.na(updated_metadata[subject, "visit_age"]) &&
-               as.numeric(updated_metadata[subject, "visit_age"]) > (age - 1) && 
-               as.numeric(updated_metadata[subject, "visit_age"]) <= age) {
-      # Add all of their data to the data frame
-      temp_df <- rbind(temp_df, subset(updated_long_newcodes, visit_age == updated_metadata[subject, "visit_age"]))
+    print("age")
+    print(age)
+    temp_df <- data.frame()
+    out_path <- paste0("age_data/bin_", age - 1, ".csv")
+    # Iterate through all subjects
+    for (subject in updated_metadata$subj) {
+            # If that subject is within the desired range
+            if (!is.na(updated_metadata[subject, "visit_age"]) &&
+                as.numeric(updated_metadata[subject, "visit_age"]) > (age - 1) && 
+                as.numeric(updated_metadata[subject, "visit_age"]) <= age) {
+                # Add all of their data to the data frame
+                temp_df <- rbind(temp_df, subset(updated_long_newcodes, 
+                    visit_age == updated_metadata[subject, "visit_age"]))
+            }
     }
-  }
-  
-  # Write each set of age data to its own csv
-  write.csv(temp_df, file = out_path)
+    
+    # Write each set of age data to its own csv
+    write.csv(temp_df, file = out_path)
 }
 
-### Next, we can compute the averages for each word for each age bin, based on all of the data across age bins ###
+# Next, we can compute the averages for each word for each age bin, 
+# based on all of the data across age bins
 
 # Read in the word statistics
 word_statistics <- read.csv("data_allsubs/wordStatistics.csv")
 
 # This function returns a data frame whose rows are words/pseudowords and whose columns are different age bins,
-# and whose cells correspond to the average accuracy for that word/pseudoword within the age bin. If there is no
-# data for a word-bin pairing, is filled with NA.
-# Takes in a data frame of word statistics, the minimum age for a bin, and the maximum age for bins.
+# and whose cells correspond to the average accuracy for that word/pseudoword 
+# within the age bin. If there is no data for a word-bin pairing, is filled with
+# NA. # Takes in a data frame of word statistics, the minimum age for a bin, 
+# and the maximum age for bins.
 get_word_age_accuracies <- function(word_statistics, min_age, max_age) {
-  # Create a vector of age bins from 7 to 28
-  age_bins <- (ceiling(min_age)-1):(ceiling(max_age)-1)
-  
-  # Create an empty data frame with words as rows and age bins as columns
-  word_age_accuracies <- data.frame(word = word_statistics$STRING, stringsAsFactors = FALSE)
-  
-  # Add columns for each age bin and initialize them with empty values
-  for (age in age_bins) {
-    bin_col <- paste("bin_", age, sep = "")
-    word_age_accuracies[, bin_col] <- ""
-  }
-  
-  # Iterate through all of the words, now in word_age_accuracies, and, iterating through all instances of that word
-  # in a given age bin, sum the accuracies (0/1) and ultimately divide by the number of occurrences
-  # to obtain the average for that word for that age bin.
-  for word 
+    # Create a vector of age bins from 7 to 28
+    age_bins <- (ceiling(min_age)-1):(ceiling(max_age)-1)
+    
+    # Create an empty data frame with words as rows and age bins as columns
+    word_age_accuracies <- data.frame(word = word_statistics$STRING, 
+        stringsAsFactors = FALSE)
+    
+    # Add columns for each age bin and initialize them with empty values
+    for (age in age_bins) {
+        bin_col <- paste("bin_", age, sep = "")
+        word_age_accuracies[, bin_col] <- ""
+    }
+    
+    # Iterate through all of the words, now in word_age_accuracies, and, 
+    # iterating through all instances of that word in a given age bin, sum the 
+    # accuracies (0/1) and ultimately divide by the number of occurrences
+    # to obtain the average for that word for that age bin.
+    for word 
 }
 
 # make a list of accuracy averages for each age bin for that word
 for word in word_statistics (would need to get this)
-  compute that accuracy by summing all of the 1s/0s for accuracy for that word in the age bin, and divide by the number of them that there are
-  can do something similar for response times
+    compute that accuracy by summing all of the 1s/0s for accuracy for that word in the age bin, and divide by the number of them that there are
+    can do something similar for response times
